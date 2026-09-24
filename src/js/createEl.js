cce7602e25dbf7348b308615a04e48a0e8bb593e
malix <288672436+Mohamed-malix@users.noreply.github.com>
2026-09-24 13:19:21 +0300

    made the default home page

--- src/js/createEl.js
+++ src/js/createEl.js
@@ -1,11 +1,19 @@
 
 
-export function createEl(element,parent,classNa){
+export function createPa(element, classNa, parent){
     let elementNa= document.createElement(`${element}`);
     elementNa.classList.add(`${classNa}`)
-    parent.appendChild(`${elementNa}`);
+    parent.appendChild(elementNa);
     console.log(elementNa)
 
     return elementNa;
 }
 
+
+export function createEl(element, classNa,text, parent){
+    let elementNa= document.createElement(`${element}`);
+    elementNa.classList.add(`${classNa}`)
+    elementNa.textContent=`${text}`;
+    parent.appendChild(elementNa);
+    console.log(elementNa)
+}
--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap';
 import '../css/style.css';
-import './createEl.js';
-import {createEl} from './createEl.js'
+import {createEl, createPa} from './createEl.js'
 console.log('App is running');
 
 
 
-let contentDiv= document.querySelector('.content');
+function defaultHome(){
+
+    let contentDiv= document.querySelector('.content');
+    let defaultContainer= createPa('div','defaultContainer',contentDiv);
+    createEl('h2', 'welcomeH2', 'Welcome to Blue-Sea Todo-list website!',defaultContainer);
+    let defaultText= createPa('div','defaultContent',defaultContainer);
+    defaultText.innerHTML=`
+    <h4 class='mt-4 mb-2'>How to use Blue-Sea todolist ? it simple</h4>
+    <ol>
+        <li>Click on the section that you want from the sidebar.</li>
+        <li>Add a task you want to finish</li>
+    </ol>
+`;
+
+}
+defaultHome();
