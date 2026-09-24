96730d6db52f3e26dcebcac2c23be9589cb48f55
malix <288672436+Mohamed-malix@users.noreply.github.com>
2026-09-24 11:38:00 +0300

    added create element function

--- src/js/createEl.js
+++ src/js/createEl.js
@@ -1,9 +1,11 @@
 
 
-export function createEl(element, parent, classNa){
+export function createEl(element,parent,classNa){
     let elementNa= document.createElement(`${element}`);
     elementNa.classList.add(`${classNa}`)
     parent.appendChild(`${elementNa}`);
+    console.log(elementNa)
 
     return elementNa;
 }
+
--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap';
 import '../css/style.css';
+import './createEl.js';
+import {createEl} from './createEl.js'
 console.log('App is running');
 
 
